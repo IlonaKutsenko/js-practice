245042473a76850ce88ea42fa4011987c1ad12a4
kutsenko.i <ilona.kutsenko@nixs.com>
2026-05-07 16:28:16 +0300

    js task

---
+++ api.js
@@ -0,0 +1,14 @@
+async function fetchPeople() {
+  const url = "https://swapi.info/api/people";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    console.log(result);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
---
+++ main.js
@@ -0,0 +1,32 @@
+import { fetchPeople } from './api.js';
+import { Person } from './person.js';
+
+let people = [];
+
+async function checkTask() {
+    const data = await fetchPeople();
+    people = data.map(item => new Person(item.name, item.height, item.mass, item.gender));
+}
+
+function getFemaleCardsFilter(personsArray) {
+    const result1 = [];
+    for (const person of personsArray) {
+        if (person.gender === 'female') {
+            const card = person.getDisplayCard();
+            result1.push(card);
+        }
+    }
+    return result1;
+}
+
+function getFemaleCardsLoop(personsArray) {
+    const result2 = [];
+    for (let i = 0; i < personsArray.length; i++) {
+        if (personsArray[i].gender === 'female') {
+            result2.push(personsArray[i].getDisplayCard());
+        }
+    }
+    return result2;
+}
+
+checkTask();
